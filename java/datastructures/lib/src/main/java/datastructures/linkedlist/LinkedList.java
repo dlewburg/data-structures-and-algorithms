da5b156e952aa86bda6ba9d2835fcc6849bd33b9
Dasha Burgos <dasha.lewis@gmail.com>
2023-04-21 17:16:59 -0400

    created skeleton code

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -2,6 +2,18 @@ package datastructures.linkedlist;
 
 public class LinkedList
 {
+  Node head = null;
+  //
+  public void insert(int value) {
+    // TODO: implement me
+    // add new node as the head of the LinkedList
+  }
+
+  public boolean includes(int value) {
+    // TODO: implement me
+    // tell weather or not there is a node with the give values in the linked list
+    return true;
+  }
   @Override
   public String toString()
     {
